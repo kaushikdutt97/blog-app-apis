1df045dd388d6c17207601c193102e10fd64a737
Kaushik Dwivedi <66638533+kaushikdutt97@users.noreply.github.com>
2024-04-25 02:00:53 +0530

    added spring security in swagger ui

--- src/main/java/com/blog/config/SwaggerConfig.java
+++ src/main/java/com/blog/config/SwaggerConfig.java
@@ -5,9 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.service.AuthorizationScope;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -20,7 +27,28 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(Arrays.asList(apiKeys()))
+                .securityContexts(securityContexts());
     }
+
+    private ApiKey apiKeys() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private List<SecurityContext> securityContexts() {
+        return Arrays.asList(SecurityContext.builder().
+                securityReferences(defaultAuth()).build());
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+
 
 }
